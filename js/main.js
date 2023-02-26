5ddce0e15aac98ee6c7faa219d132e09ffe77abe
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-02-26 14:17:26 +0100

    pracs-film

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 const KEY = "345007f9ab440e5b86cef51be6397df1";
 
-fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`)
-  .then((res) => {
+const box = document.querySelector(".box");
+const trailerBox = document.querySelector(".trailer");
+const guard = document.querySelector(".guard");
+
+let options = {
+  root: null,
+  rootMargin: "100px",
+  threshold: 1.0,
+};
+
+let page = 1;
+
+let observer = new IntersectionObserver(callback, options);
+
+function onFetchMovies(page = 1) {
+  return fetch(
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}&page=${page}`
+  ).then((res) => {
     if (!res.ok) {
       throw new Error("error");
     }
     return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    createCard(data.results);
   });
+  // .then((data) => {
+  //   console.log(data);
+  //   createCard(data.results);
+  // });
+}
 
-// fetch(`https://api.themoviedb.org/3/movie/505642/movie?api_key=${KEY}`)
+onFetchMovies().then((data) => {
+  createCard(data.results);
+
+  observer.observe(guard);
+});
+// fetch(`https://api.themoviedb.org/3/movie/id/videos?api_key=${KEY}`)
 //   .then((res) => {
 //     if (!res.ok) {
 //       throw new Error("error");
@@ -24,15 +46,14 @@ fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`)
 //     createCard(data.results);
 //   });
 
-const box = document.querySelector(".box");
 box.addEventListener("click", onClick);
 
 function createCard(arr) {
   const marcup = arr
     .map(({ id, title, poster_path }) => {
-      return `<li data-id="${id}">
-        <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}">
-        <p>${title}</p>
+      return `<li class="item" data-id="${id}">
+        <img class="test" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}">
+        <p class="test">${title}</p>
     </li>`;
     })
     .join("");
@@ -40,4 +61,43 @@ function createCard(arr) {
   box.insertAdjacentHTML("beforeend", marcup);
 }
 
-function onClick(e) {}
+function onClick(e) {
+  if (e.target.classList.contains("test")) {
+    const item = e.target.closest("li");
+    const { id } = item.dataset;
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${KEY}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("error");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        onClickPlayer(data.results);
+      });
+  }
+}
+
+function onClickPlayer(arr) {
+  const item = arr.find(
+    ({ site, type }) => site === "YouTube" && type === "Trailer"
+  );
+  const instance = basicLightbox.create(
+    `<div><iframe src="https://www.youtube.com/embed/${item.key}"</div>`
+  );
+
+  instance.show();
+}
+
+function callback(entries) {
+  if (entries[0].isIntersecting) {
+    page += 1;
+    onFetchMovies(page).then((data) => {
+      createCard(data.results);
+      if (data.total_pages === page) {
+        observer.unobserve(guard);
+      }
+    });
+  }
+}
